71d7d94be7303b43a9bbb26226d95c27fd63ef8f
splx <splaandrey@gmail.com>
2023-07-18 23:26:59 +0300

    Added calculate function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-fn main() {
-    let foo_number = 5;
+fn calculate_sum(numbers: &[i32]) -> Result<i32, String> {
+    let sum = numbers.iter().sum();
+    Ok(sum)
+}
 
-    println!("{foo_number}");
-    println!("Hello, Rust!!!");
+fn main() {
+    let numbers = vec![1, 2, 3, 4, 5];
+    match calculate_sum(&numbers) {
+        Ok(sum) => println!("Sum: {}", sum),
+        Err(e) => println!("Error: {}", e),
+    }
 }
